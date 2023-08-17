9a729f31c026560468390a0f38845e18ea5c9a18
Austen Cloud <austencloud@gmail.com>
2023-08-17 09:34:25 -0400

    made json editable when values change

--- components/json_editor.py
+++ components/json_editor.py
@@ -1,30 +1,66 @@
-
-from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, QMenuBar, QFileDialog
+from PyQt5.QtWidgets import QLabel, QMainWindow, QWidget, QApplication, QAction, QMenuBar, QFileDialog
+from PyQt5.QtWidgets import QPushButton, QVBoxLayout
+from PyQt5.QtCore import QTimer, Qt
 from components.context_menu import ContextMenu, ContextMenuItem
 from components.json_tree import JsonTree
 from components.code_editor import CodeEditor
 from components.split_panel import SplitPanel
 import json
 
+
 class JsonEditor(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
+        
+        self.resize(800, 1200)
+
         self.json_data = {}
         self.json_tree = JsonTree()
         self.code_editor = CodeEditor()
+        self.current_file_path = None  # To keep track of the source file
+
+        self.save_status_label = QLabel("")
+        self.save_status_label.setMaximumHeight(16)
+        self.save_status_label.setStyleSheet("font: 12pt")
+        self.save_status_label.setAlignment(Qt.AlignCenter)
+
         self.split_panel = SplitPanel(self.json_tree, self.code_editor)
         self.split_panel.set_sizes([300, 500])
 
-        self.setCentralWidget(self.split_panel)
+        layout = QVBoxLayout()
+        layout.addWidget(self.save_status_label)  # Removed self.save_button
+        layout.addWidget(self.split_panel)
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
         self.create_menu_bar()
         self.connect_signals()
+        self.load_default_file()
+
+    def load_default_file(self):
+        self.current_file_path = 'pictographs.json'  # Set the path of the default file
+        try:
+            with open('pictographs.json', 'r') as file:
+                self.json_data = json.load(file)  # Update this line
+                self.json_tree.set_json(self.json_data)
+                self.code_editor.set_code(json.dumps(self.json_data, indent=4))
+        except Exception as e:
+            print(f"Failed to load default file: {e}")
 
     def create_menu_bar(self):
         menu_bar = QMenuBar()
         file_menu = menu_bar.addMenu("File")
+        
         open_action = QAction("Open", self)
         open_action.triggered.connect(self.open_json_file)
         file_menu.addAction(open_action)
+
+        save_action = QAction("Save", self)
+        save_action.triggered.connect(self.save_json_file)
+        file_menu.addAction(save_action)
+
         self.setMenuBar(menu_bar)
 
     def connect_signals(self):
@@ -33,8 +69,9 @@ class JsonEditor(QMainWindow):
     def on_json_tree_item_changed(self, item, column):
         path = self.json_tree.get_path_to_item(item)
         value = item.text(column)
-        self.json_tree.update_json_data(path, value)
+        self.json_tree.update_json_data(self.json_data, path, value)
         self.code_editor.set_code(json.dumps(self.json_data, indent=4))
+        self.save_json_file()  # Save the JSON file immediately after the edit
 
     def open_json_file(self):
         options = QFileDialog.Options()
@@ -42,7 +79,13 @@ class JsonEditor(QMainWindow):
         if file_path:
             with open(file_path, 'r') as file:
                 self.json_data = json.load(file)
-                self.json_tree.json_data = self.json_data
-                self.json_tree.clear()
-                self.json_tree.add_subnodes(None, self.json_data)
-                self.code_editor.set_code(json.dumps(self.json_data, indent=4)) 
+                self.current_file_path = file_path  
+                self.json_tree.set_json(self.json_data)  # Update this line
+                self.code_editor.set_code(json.dumps(self.json_data, indent=4))
+                
+    def save_json_file(self):
+        if self.current_file_path:
+            with open(self.current_file_path, 'w') as file:
+                json.dump(self.json_data, file, indent=4)
+            self.save_status_label.setText("File Saved!")
+            QTimer.singleShot(5000, lambda: self.save_status_label.setText(""))  # Clear message after 5 seconds
--- components/json_tree.py
+++ components/json_tree.py
@@ -1,5 +1,8 @@
-from PyQt5.QtCore import Qt, pyqtSignal
+from PyQt5.QtCore import Qt, pyqtSignal, QTimer
 from PyQt5.QtWidgets import QTreeWidget, QTreeWidgetItem
+from PyQt5.QtGui import QFont, QColor, QBrush
+from functools import partial
+
 
 
 class JsonTree(QTreeWidget):
@@ -8,16 +11,47 @@ class JsonTree(QTreeWidget):
     def __init__(self, parent=None):
         super(JsonTree, self).__init__(parent)
         self.itemChanged.connect(self.on_item_changed)
-        self.json_data = {}  # Initialize json_data here
-
+        self.itemCollapsed.connect(self.on_item_collapsed)
+        self.json_data = {}
 
     def set_json(self, json_data):
         self.json_data = json_data
         self.clear()
         self.add_subnodes(None, json_data)
-
+        
     def add_subnodes(self, parent, data, path=[]):
-        if isinstance(data, dict):
+        if isinstance(data, list):
+            for idx, group in enumerate(data):
+                # Extract start and end positions
+                start_position = group[0]["start_position"].replace("alpha", "α").replace("beta", "β").replace("gamma", "Γ")
+                end_position = group[0]["end_position"].replace("alpha", "α").replace("beta", "β").replace("gamma", "Γ")
+
+                # Create a new item with the start and end positions
+                item = QTreeWidgetItem([f"{start_position}→{end_position}"])
+                item.setData(0, Qt.UserRole, path + [idx])
+                if parent is None:
+                    self.addTopLevelItem(item)
+                else:
+                    parent.addChild(item)
+
+                for sub_idx, sub_item in enumerate(group[1:]):
+                    sub_key = ["Blue", "Red", "Optimal Location"][sub_idx] if sub_idx < 3 else str(sub_idx)
+                    child_item = QTreeWidgetItem([sub_key])
+                    child_item.setData(0, Qt.UserRole, path + [idx, sub_idx + 1])  # Use sub_idx + 1 instead of sub_key
+                    if sub_key in ["Red", "Blue"]:
+                        font = QFont()
+                        font.setBold(True)
+                        child_item.setFont(0, font)
+                        color = QColor("red") if sub_key == "Red" else QColor("blue")
+                        child_item.setForeground(0, QBrush(color))
+                    item.addChild(child_item)
+
+                    if isinstance(sub_item, dict):
+                        self.add_subnodes(child_item, sub_item, path + [idx, sub_idx + 1])  # Corrected path here
+                        if sub_key in ["Red", "Blue"]:
+                            self.collapse_to_leaf_nodes(child_item)
+
+        elif isinstance(data, dict):
             for key, value in data.items():
                 item = QTreeWidgetItem([key])
                 item.setData(0, Qt.UserRole, path + [key])
@@ -26,22 +60,26 @@ class JsonTree(QTreeWidget):
                 else:
                     parent.addChild(item)
                 self.add_subnodes(item, value, path + [key])
-        elif isinstance(data, list):
-            for idx, value in enumerate(data):
-                item = QTreeWidgetItem([f"[{idx}]"])
-                item.setData(0, Qt.UserRole, path + [idx])
-                if parent is None:
-                    self.addTopLevelItem(item)
-                else:
-                    parent.addChild(item)
-                self.add_subnodes(item, value, path + [idx])
         else:
             item = QTreeWidgetItem([str(data)])
             item.setData(0, Qt.UserRole, path)
+            item.setFlags(item.flags() | Qt.ItemIsEditable)  # Making the item editable
             if parent is None:
                 self.addTopLevelItem(item)
             else:
                 parent.addChild(item)
+
+    def collapse_to_leaf_nodes(self, item):
+        for i in range(item.childCount()):
+            child = item.child(i)
+            self.collapse_to_leaf_nodes(child)
+            self.collapseItem(child)
+
+    def on_item_collapsed(self, item):
+        # Auto-collapse all sub-levels of the collapsed item
+        for i in range(item.childCount()):
+            child = item.child(i)
+            self.collapseItem(child)
 
     def get_path_to_item(self, item):
         # Retrieve the path from the custom data
@@ -49,17 +87,20 @@ class JsonTree(QTreeWidget):
 
     def on_item_changed(self, item, column):
         if item.childCount() == 0:  # It's a leaf node
-            path = self.get_path_to_item(item)
+            path = item.data(0, Qt.UserRole)
             value = item.text(column)
-            self.update_json_data(path, value)
-            self.itemValueChanged.emit(path, value)  # Emit the signal
+            self.update_json_data(self.json_data, path, value)
+    
+    def update_json_data(self, json_data, path, value):
+        # Access the outer list using the first key
+        outer_list = json_data[path[0]]
 
-    def update_json_data(self, path, value):
-        target_data = self.json_data
-        for key in path[:-1]:
-            if isinstance(key, int):
-                target_data = target_data[key]
-            else:
-                target_data = target_data[key]
-        last_key = path[-1]
-        target_data[last_key] = value
+        # Access the inner list using the second key
+        inner_list = outer_list[path[1]]
+
+        # Access the color object using the third key
+        color_object = inner_list[path[2]]
+
+        # Update the value using the fourth key
+        color_object[path[-1]] = value
+
